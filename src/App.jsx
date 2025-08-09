60e8614eadd037a98b17fba6c20725b477f9e0ab
Nuzul <farhan.nuzul@ui.ac.id>
2025-08-09 13:01:14 +0800

    Initial UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Search from "./components/Search.jsx";
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('I am a Batman');
+
+
     return (
-        <div>App</div>
+        <main>
+            <div className="pattern"/>
+
+            <div className="wrapper">
+                <img src="./hero.png" alt="Hero Banner" />
+                <header>
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+                </header>
+
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </div>
+        </main>
     )
 }
 export default App
---
+++ src/components/Search.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Search = (props) => {
+    return (
+        <div className="text-white text-3xl">{props.searchTerm}</div>
+    )
+}
+export default Search
